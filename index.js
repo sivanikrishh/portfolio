52ac53b20b1a80709ca9585a6f30f4a57c6d4455
sivanikrishh <sivanikrishna13@gmail.com>
2025-09-25 10:08:05 +0530

    Updated software development skill description

---
+++ index.js
@@ -0,0 +1,23 @@
+
+function sendMail(event) {
+    event.preventDefault();
+  
+    var params = {
+      name: document.getElementById("inputName").value,
+      email: document.getElementById("inputEmail").value,
+      message: document.getElementById("inputMessage").value,
+    };
+  
+    const serviceID = "service_qd2mkla";
+    const templateID = "template_zfqrbkd";
+  
+    emailjs.send(serviceID, templateID, params)
+      .then(res => {
+        document.getElementById("inputName").value = "";
+        document.getElementById("inputEmail").value = "";
+        document.getElementById("inputMessage").value = "";
+        console.log(res);
+        alert("Your message was sent successfully");
+      })
+      .catch(err => console.log(err));
+  }
